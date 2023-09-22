329494bb4baef9cb3945925ce3f48f54100e8da3
Jayson Chiang <jayson.chiang@outlook.com>
2023-09-22 15:34:00 +0800

    added dashboard

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import Dashboard from './components/Dashboard.vue';
+import Dashboard from './components/Dashboard';
 
 // Mount function to start up the app
 const mount = (el) => {
